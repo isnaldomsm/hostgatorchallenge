ab10e7576300fd3bc784c75ec8ce165b1cb5e356
Isnaldo Souza <isnaldomsm@gmail.com>
2019-05-14 11:26:17 -0300

    config routes

--- config/routes.php
+++ config/routes.php
@@ -1,1 +1,10 @@
 <?php
+use App\Controllers\BloodController;
+
+$app->group('',
+    //BloodController function
+    function () {
+
+        $this->get('/breeds', BloodController::class . ':search');
+        $this->get('/breeds/{ID}', BloodController::class . ':show');
+});
