a2e16234a92f4c4823bcb8f59615286a3853bea5
Isnaldo Souza <isnaldomsm@gmail.com>
2019-05-14 09:31:11 -0300

    generic class, pimple. Default credentials api

--- config/dependencies.php
+++ config/dependencies.php
@@ -6,3 +6,26 @@ $container = $app->getContainer();
 
 
 $container->register(new \App\Services\DatabaseServices\EloquentServiceProvider());
+
+
+$container['logger'] = function ($c) {
+	$settings = $c->get('settings')['logger'];
+	$logger = new Monolog\Logger($settings['name']);
+	$logger->pushProcessor(new Monolog\Processor\UidProcessor());
+	$logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
+	return $logger;
+};
+
+
+ $container['thecatapi'] = function ($c) {
+	$settings = $c->get('settings')['thecatapi'];
+
+	// default  credentials
+	$genericClass = new stdClass();
+
+	// set values at classes properties
+	$genericClass->{'apiKey'} = $settings['apiKey'];
+	$genericClass->{'apiUrl'} = $settings['apiUrl'];
+
+	return $genericClass;
+};
