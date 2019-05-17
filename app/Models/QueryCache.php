c8622665c691b8eb5be0ac679fb603916bbb423c
Isnaldo Souza <isnaldomsm@gmail.com>
2019-05-17 09:46:15 -0300

    rrefactoring cache

--- app/Models/QueryCache.php
+++ app/Models/QueryCache.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
  * @property \Carbon\Carbon                           created_at
  * @property \Carbon\Carbon                           update_at
  */
+ //layer cache
 class QueryCache extends Model{
 	public $table = "query_cache";
 }
